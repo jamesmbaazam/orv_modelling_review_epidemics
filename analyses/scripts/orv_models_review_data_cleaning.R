f12e76b327b837aec8d081457eb046969ff33778
James Azam <james.azam@lshtm.ac.uk>
2023-04-26 19:06:23 +0100

    Moved the extra data cleaning steps from the main analysis script here

--- analyses/scripts/orv_models_review_data_cleaning.R
+++ analyses/scripts/orv_models_review_data_cleaning.R
@@ -226,11 +226,95 @@ citation_data_cleaned <- citation_data %>%
     year = as.numeric(year)
   )
 
-# COmbine the citation database and cleaned dataset
+# Combine the citation database and cleaned dataset
 compact_data_with_citation_keys <- left_join(review_data_compact_cleaned,
   citation_data_cleaned,
   by = c("title")
 )
+
+
+# Clean individual variables
+compact_data_with_citation_keys <- compact_data_with_citation_keys %>%
+  mutate(publication_year = as.numeric(publication_year),
+         author_in_country_studied = as.character(author_in_country_studied)
+         ) %>%
+  arrange(publication_year) %>%
+  mutate(vax_modelled = ifelse(
+    str_detect(intervention_modelled, 'vaccination'), 'yes', 'no')) %>%
+  mutate(
+    intervention_type = case_when(
+    # When vax was not modelled at all
+    vax_modelled == 'no' & vax_modelled_with_non_vax == 'no' ~ 'no_vax',
+    # When vax was modelled but not for comparison as a single intervention
+    vax_modelled == 'yes' & vax_modelled_with_non_vax == 'no' ~ 'vax_combination_with_others',
+    # When vax was modelled as a single intervention to be compared with others
+    vax_modelled == 'yes' & vax_modelled_with_non_vax == 'yes' ~ 'vax_single'
+    )
+    ) %>%
+  mutate(
+    author_affiliation_type = str_replace_all(author_affiliation_type, ' ', ' + ')
+    ) |>
+  mutate(
+    author_in_country_studied = if_else(author_in_country_studied == 'NA',
+                                        'not_applicable',
+                                        author_in_country_studied
+                                        )
+    ) %>%
+  #Aggregate years before 2006 into one group because of the low number of papers
+  mutate(year_aggreg = as.numeric(
+    ifelse(publication_year < 2006, 2005, publication_year)
+    )
+    ) |>
+  mutate(
+  collab_type = as_factor(
+    if_else(author_affiliation_type == 'academic_institutions',
+            'purely_academic',
+            'mixed')
+    ),
+  purely_academic_collab = if_else(
+    author_affiliation_type == 'academic_institutions', TRUE, FALSE)
+  ) |>
+  relocate(purely_academic_collab, .after = author_affiliation_type)
+
+# Clean descriptions of parametrization and validation with shorter labels
+
+#' First create dictionary of parametrization methods descriptions and shorter
+#' replacements
+
+parametrization_dict <- c('values_from_literature' = 'literature',
+                          'values_from_guess_and_expert_opion' = 'expert_opinion',
+                          'values_fitted_to_ts' = 'fitted',
+                          ' ' = ' and '
+                          )
+
+# Dictionary of validation methods descriptions
+validation_dict <- c('compared_with_data' = 'data',
+                     'compared_with_other_model_output' = 'another_model',
+                     'model_not_validated' = 'none',
+                     ' ' = '_and_'
+                     )
+
+compact_data_with_citation_keys <- compact_data_with_citation_keys %>%
+  mutate(parametrization = str_replace_all(parametrization,
+                                           parametrization_dict
+                                           )
+         ) %>%
+  mutate(validation = str_replace_all(validation, validation_dict)) %>%
+  mutate(model_structure = str_replace(model_structure,
+                                       'deterministic stochastic',
+                                       'both'
+                                       )
+         )
+
+
+#Rearrange the columns
+compact_data_with_citation_keys <- compact_data_with_citation_keys %>%
+  relocate(c(title, bibtexkey), .before = publication_year) %>%
+  relocate(publication_year, .before = bibtexkey) %>%
+  relocate(c(x0_reviewer, category, author:language),
+           .after = collab_type
+  ) |>
+  arrange(title)
 
 
 
